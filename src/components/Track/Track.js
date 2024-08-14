0a3f98b9f5dfa5bdcabe5d9e505cfbe57e456c14
vermilt <canmilton@gmail.com>
2024-08-14 22:33:41 +0200

    Track object array added to Track.js

---
+++ src/components/Track/Track.js
@@ -0,0 +1,353 @@
+import React {useState} from "react";
+
+const tracks = [
+    {
+        id: 1,
+        name: "Roygbiv",
+        artist: "Boards of Canada",
+        album: "Music Has the Right to Children"
+    },
+    {
+        id: 2,
+        name: "Dayvan Cowboy",
+        artist: "Boards of Canada",
+        album: "The Campfire Headphase"
+    },
+    {
+        id: 3,
+        name: "Telephasic Workshop",
+        artist: "Boards of Canada",
+        album: "Music Has the Right to Children"
+    },
+    {
+        id: 4,
+        name: "Aquarius",
+        artist: "Boards of Canada",
+        album: "Music Has the Right to Children"
+    },
+    {
+        id: 5,
+        name: "Chromakey Dreamcoat",
+        artist: "Boards of Canada",
+        album: "The Campfire Headphase"
+    },
+    {
+        id: 6,
+        name: "An Eagle in Your Mind",
+        artist: "Boards of Canada",
+        album: "Music Has the Right to Children"
+    },
+    {
+        id: 7,
+        name: "Everything You Do Is a Balloon",
+        artist: "Boards of Canada",
+        album: "Hi Scores"
+    },
+    {
+        id: 8,
+        name: "In a Beautiful Place Out in the Country",
+        artist: "Boards of Canada",
+        album: "In a Beautiful Place Out in the Country"
+    },
+    {
+        id: 9,
+        name: "Rue the Whirl",
+        artist: "Boards of Canada",
+        album: "Music Has the Right to Children"
+    },
+    {
+        id: 10,
+        name: "Music Is Math",
+        artist: "Boards of Canada",
+        album: "Geogaddi"
+    },
+    {
+        id: 11,
+        name: "A Warm Place",
+        artist: "Nine Inch Nails",
+        album: "The Downward Spiral"
+    },
+    {
+        id: 12,
+        name: "Windowlicker",
+        artist: "Aphex Twin",
+        album: "Windowlicker"
+    },
+    {
+        id: 13,
+        name: "Xtal",
+        artist: "Aphex Twin",
+        album: "Selected Ambient Works 85-92"
+    },
+    {
+        id: 14,
+        name: "Girl/Boy Song",
+        artist: "Aphex Twin",
+        album: "Richard D. James Album"
+    },
+    {
+        id: 15,
+        name: "Flim",
+        artist: "Aphex Twin",
+        album: "Come to Daddy"
+    },
+    {
+        id: 16,
+        name: "Roy's Peak",
+        artist: "Tycho",
+        album: "Weather"
+    },
+    {
+        id: 17,
+        name: "A Walk",
+        artist: "Tycho",
+        album: "Dive"
+    },
+    {
+        id: 18,
+        name: "L",
+        artist: "Tycho",
+        album: "Epoch"
+    },
+    {
+        id: 19,
+        name: "Awake",
+        artist: "Tycho",
+        album: "Awake"
+    },
+    {
+        id: 20,
+        name: "Open Eye Signal",
+        artist: "Jon Hopkins",
+        album: "Immunity"
+    },
+    {
+        id: 21,
+        name: "We Disappear",
+        artist: "Jon Hopkins",
+        album: "Immunity"
+    },
+    {
+        id: 22,
+        name: "Emerald and Stone",
+        artist: "Brian Eno",
+        album: "Small Craft on a Milk Sea"
+    },
+    {
+        id: 23,
+        name: "On Some Faraway Beach",
+        artist: "Brian Eno",
+        album: "Here Come the Warm Jets"
+    },
+    {
+        id: 24,
+        name: "Cherry",
+        artist: "Underworld",
+        album: "Second Toughest in the Infants"
+    },
+    {
+        id: 25,
+        name: "Born Slippy .NUXX",
+        artist: "Underworld",
+        album: "Trainspotting (Original Motion Picture Soundtrack)"
+    },
+    // 200 additional tracks below:
+    {
+        id: 26,
+        name: "Hyperballad",
+        artist: "Björk",
+        album: "Post"
+    },
+    {
+        id: 27,
+        name: "Teardrop",
+        artist: "Massive Attack",
+        album: "Mezzanine"
+    },
+    {
+        id: 28,
+        name: "Angel",
+        artist: "Massive Attack",
+        album: "Mezzanine"
+    },
+    {
+        id: 29,
+        name: "Risingson",
+        artist: "Massive Attack",
+        album: "Mezzanine"
+    },
+    {
+        id: 30,
+        name: "Glory Box",
+        artist: "Portishead",
+        album: "Dummy"
+    },
+    {
+        id: 31,
+        name: "Numb",
+        artist: "Portishead",
+        album: "Dummy"
+    },
+    {
+        id: 32,
+        name: "Sour Times",
+        artist: "Portishead",
+        album: "Dummy"
+    },
+    {
+        id: 33,
+        name: "Only You",
+        artist: "Portishead",
+        album: "Portishead"
+    },
+    {
+        id: 34,
+        name: "Machine Gun",
+        artist: "Portishead",
+        album: "Third"
+    },
+    {
+        id: 35,
+        name: "The Rip",
+        artist: "Portishead",
+        album: "Third"
+    },
+    {
+        id: 36,
+        name: "Angel Echoes",
+        artist: "Four Tet",
+        album: "There Is Love in You"
+    },
+    {
+        id: 37,
+        name: "Love Cry",
+        artist: "Four Tet",
+        album: "There Is Love in You"
+    },
+    {
+        id: 38,
+        name: "Sing",
+        artist: "Four Tet",
+        album: "There Is Love in You"
+    },
+    {
+        id: 39,
+        name: "Aerials",
+        artist: "System of a Down",
+        album: "Toxicity"
+    },
+    {
+        id: 40,
+        name: "Toxicity",
+        artist: "System of a Down",
+        album: "Toxicity"
+    },
+    {
+        id: 41,
+        name: "B.Y.O.B.",
+        artist: "System of a Down",
+        album: "Mezmerize"
+    },
+    {
+        id: 42,
+        name: "Sugar",
+        artist: "System of a Down",
+        album: "System of a Down"
+    },
+    {
+        id: 43,
+        name: "Chop Suey!",
+        artist: "System of a Down",
+        album: "Toxicity"
+    },
+    {
+        id: 44,
+        name: "Breathe",
+        artist: "The Prodigy",
+        album: "The Fat of the Land"
+    },
+    {
+        id: 45,
+        name: "Firestarter",
+        artist: "The Prodigy",
+        album: "The Fat of the Land"
+    },
+    {
+        id: 46,
+        name: "Smack My Bitch Up",
+        artist: "The Prodigy",
+        album: "The Fat of the Land"
+    },
+    {
+        id: 47,
+        name: "Voodoo People",
+        artist: "The Prodigy",
+        album: "Music for the Jilted Generation"
+    },
+    {
+        id: 48,
+        name: "Poison",
+        artist: "The Prodigy",
+        album: "Music for the Jilted Generation"
+    },
+    {
+        id: 49,
+        name: "Around the World",
+        artist: "Daft Punk",
+        album: "Homework"
+    },
+    {
+        id: 50,
+        name: "Da Funk",
+        artist: "Daft Punk",
+        album: "Homework"
+    },
+    {
+        id: 51,
+        name: "One More Time",
+        artist: "Daft Punk",
+        album: "Discovery"
+    },
+    {
+        id: 52,
+        name: "Digital Love",
+        artist: "Daft Punk",
+        album: "Discovery"
+    },
+    {
+        id: 53,
+        name: "Harder, Better, Faster, Stronger",
+        artist: "Daft Punk",
+        album: "Discovery"
+    },
+    {
+        id: 54,
+        name: "D.A.N.C.E.",
+        artist: "Justice",
+        album: "Cross"
+    },
+    {
+        id: 55,
+        name: "Phantom Pt. II",
+        artist: "Justice",
+        album: "Cross"
+    },
+    {
+        id: 56,
+        name: "Genesis",
+        artist: "Justice",
+        album: "Cross"
+    },
+    {
+        id: 57,
+        name: "We Are Your Friends",
+        artist: "Justice vs. Simian",
+        album: "We Are Your Friends"
+    },
+    {
+        id: 58,
+        name: "Stress",
+        artist: "Justice",
+        album: "Cross"
+    }
+];
