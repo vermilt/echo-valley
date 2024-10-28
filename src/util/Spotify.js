904d09d21a6e3886204a39f34ebdfd61a26cdb6e
vermilt <canmilton@gmail.com>
2024-10-28 22:03:05 +0100

    GH redirect changed

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,7 +1,7 @@
 let accessToken;
 var client_id = '2c2045eba076409ea93090758c1873c9';
-// var redirect_uri = 'https://vermilt.github.io/echo-valley/callback';
-var redirect_uri =  'http://localhost:3000/callback';
+var redirect_uri = 'https://vermilt.github.io/echo-valley/callback';
+//var redirect_uri =  'http://localhost:3000/callback';
 var scope = 'playlist-modify-private playlist-modify-public user-read-private user-read-email';
 
 const Spotify = {
